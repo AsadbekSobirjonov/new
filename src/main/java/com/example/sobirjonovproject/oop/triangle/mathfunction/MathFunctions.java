9e50cec1bc3662b586a91b8d63e4dd14bfd71b14
sjv18 <asobirjonov.unlimit@gmail.com>
2023-11-08 15:58:04 +0500

    opp

---
+++ src/main/java/com/example/sobirjonovproject/oop/triangle/mathfunction/MathFunctions.java
@@ -0,0 +1,23 @@
+package com.example.sobirjonovproject.oop.triangle.mathfunction;
+
+public class MathFunctions {
+    public Double add(int a, int b){
+        return (double)a+b;
+    }
+    public Double sub(int a, int b){
+        return (double) a-b;
+    }
+    public Double multiply(int a, int b){
+        return (double) a*b;
+    }
+    public Double div(int a, int b){
+        return (double) a/b;
+    }
+    public Integer abc(Integer a){
+        return Math.abs(a);
+    }
+    public Integer pow(Integer a){
+        return a*a;
+    }
+
+}
